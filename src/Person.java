1ee5a6b2600ef2ae2a8f6204bdbe634822491320
Shahana0333 <shanasha0333@gmail.com>
2025-06-18 17:44:06 +0530

    first commit

---
+++ src/Person.java
@@ -0,0 +1,14 @@
+
+public class Person {
+	String name;
+	int age;
+	String number;
+	
+	void printDetails()
+	{
+		System.out.println("Name : "+name);
+		System.out.println("Age : "+age);
+		System.out.println("Number : "+number);
+		
+	}
+}
---
+++ src/PersonDetails.java
@@ -0,0 +1,31 @@
+
+public class PersonDetails {
+
+	public static void main(String[] args) {
+		
+		Person Shahana=new Person();
+		Shahana.name="Shahana";
+		Shahana.age=23;
+		Shahana.number="76534258986";
+		Shahana.printDetails();
+		
+		Person Shanees=new Person();
+		Shanees.name="Shanees";
+		Shanees.age=24;
+		Shanees.number="8900065421";
+		Shanees.printDetails();
+		
+		Person Rahul=new Person();
+		Rahul.name="Rahul";
+		Rahul.age=32;
+		Rahul.number="3219908765";
+		Rahul.printDetails();
+		
+		Person Seetha=new Person();
+		Seetha.name="Seetha";
+		Seetha.age=27;
+		Seetha.number="8900641112";
+		Seetha.printDetails();
+	}
+
+}
