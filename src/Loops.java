c6bbcfc91a5c38ad114e2ae066a10cda836a49ca
Shahana0333 <shanasha0333@gmail.com>
2025-06-23 16:54:19 +0530

    Excersise Loop

---
+++ src/Loops.java
@@ -0,0 +1,27 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+
+		// FOR LOOP
+
+		for (int i = 1; i <= 20; i++) {
+			System.out.println(i);
+		}
+
+		// FOR LOOP PRINT IN REVERSE
+
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+
+		// WHILE
+
+		int i = 10;
+
+		while (i >= 1) {
+			System.out.println(i);
+			i--;
+		}
+	}
+}
